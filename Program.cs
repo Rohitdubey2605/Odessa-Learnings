a2ecc94c6248aa44b40ca3f5c3e38424dc905ffe
Rohit Dubey <Rohit.Dubey@odessainc.com>
2018-10-05 17:08:01 +0530

    Array Code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace passing_array
+{
+    class Program
+    {
+        int Add(params int[] arr)
+        {
+            int s = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                s += arr[i];
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            int[] arr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int c = a.Add(arr);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
